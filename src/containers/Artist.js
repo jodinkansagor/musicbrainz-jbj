24a2a545d13fd198a58e7cb6355d4d3804bd2f68
Jodi N Kansagor <joedvice@gmail.com>
2020-02-08 16:53:22 -0800

    styling more finished

--- src/containers/Artist.js
+++ src/containers/Artist.js
@@ -12,6 +12,11 @@ const Artist = () => {
   return (
     <section className={styles.Container}>
       <ArtistInput className={styles.input} artistName={artistName} onChange={handleChange} onSubmit={handleSubmit} />
+      <If condition={artistArray.length === 0}>
+        <section className={styles.image}>
+          <img src="https://i.pinimg.com/originals/69/05/ac/6905acb4cc2ad562cf0ebb79366bbf74.jpg" />
+        </section>
+      </If>
       <ArtistList artistArray={artistArray} artistName={artistName} />
       <If condition={artistArray.length > 0}>
         <Paging handlePreviousClick={handlePreviousClick} handleNextClick={handleNextClick} currentPage={currentPage} />
