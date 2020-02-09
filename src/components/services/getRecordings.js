2af6de9bd13b72df1560ce53eeed7f104376b469
Jodi N Kansagor <joedvice@gmail.com>
2020-02-08 18:35:14 -0800

    changed to https

--- src/components/services/getRecordings.js
+++ src/components/services/getRecordings.js
@@ -1,5 +1,5 @@
 const getRecordings = albumId => {
-  return fetch(`http://musicbrainz.org/ws/2/recording?release=${albumId}&fmt=json`)
+  return fetch(`https://musicbrainz.org/ws/2/recording?release=${albumId}&fmt=json`)
     .then(res => res.json())
     .then(({ recordings }) => recordings.map(recording => ({
       recordingTitle: recording.title.replace(/\s/g, '-')
